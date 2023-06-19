c73e97794c9fa95ff2518b7bf4f859f8a117dd5c
sukruu <31570515+sukruu@users.noreply.github.com>
2023-06-19 21:52:24 +0300

    Asp.Net-Mvc-Identity
    Identity ile üyelik işlemleri

---
+++ UserIdentity/Controllers/AccountController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserIdentity.Identity;
+using UserIdentity.Models;
+
+namespace UserIdentity.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            userManager = new UserManager<ApplicationUser>(userStore);
+
+            userManager.PasswordValidator = new CustomPasswordValidator()
+            {
+                RequireDigit = true,
+                RequiredLength = 7,
+                RequireLowercase = true,
+                RequireUppercase = true,
+                RequireNonLetterOrDigit = true
+            };
+
+            userManager.UserValidator = new UserValidator<ApplicationUser>(userManager)
+            {
+                RequireUniqueEmail = true,
+                AllowOnlyAlphanumericUserNames = false
+            };
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return View("Error", new string[] { "Erişim hakkınız yok" });
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.Find(model.Username, model.Password);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Yanlış kullanıcı adı veya parola");
+                }
+                else
+                {
+                    var authManager = HttpContext.GetOwinContext().Authentication;
+
+                    var identity = userManager.CreateIdentity(user, "ApplicationCookie");
+                    var authProperties = new AuthenticationProperties()
+                    {
+                        IsPersistent = true
+                    };
+
+                    authManager.SignOut();
+                    authManager.SignIn(authProperties, identity);
+
+                    return Redirect(string.IsNullOrEmpty(returnUrl) ? "/" : returnUrl);
+                }
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.UserName = model.Username;
+                user.Email = model.Email;
+
+                var result = userManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "User");
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+
+            return RedirectToAction("Login");
+        }
+
+    }
+}
